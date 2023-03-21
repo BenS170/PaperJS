16448af141cdc4cb83b01d870755420c839bc583
BenS170 <Ben@spence.vip>
2023-03-21 13:37:18 -0500

    Problem 2 solved

---
+++ sketch.js
@@ -0,0 +1,12 @@
+
+// Rectangle
+
+var origin = new Point(200, 100);
+var rectSize = new Size(250, 150);
+
+var myPath = new Path.Rectangle(origin, rectSize);
+myPath.strokeColor = 'gold';
+myPath.strokeWidth = 10;
+myPath.fillColor = 'maroon';
+
+myPath.segments[3].remove();
