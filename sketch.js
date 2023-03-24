5204fb32f3fb2e28a499af09739e26138d3ea22b
BenS170 <Ben@spence.vip>
2023-03-24 11:09:40 -0500

    Added code for problem 4

--- sketch.js
+++ sketch.js
@@ -1,21 +1,62 @@
+// random points
+var x = [20, 120, 220, 320, 420, 520, 620, 720, 820, 920, 1020, 1120, 1220, 1320];
+var y = [966, 961, 886, 928, 908, 888, 894, 926, 930, 914, 917, 934, 945, 906];
 
-var circle = new Shape.Circle(new Point(200, 150), 50);
-circle.fillColor = 'black';
-var text = new PointText({
-    point: [0, 50],
-    content: 'x: 100, y: 100',
-    fillColor: 'black',
-    fontFamily: 'Courier New',
-    fontWeight: 'bold',
-    fontSize: 20
-});
+// declare a new path
+//*** your code ***//
+var path = new Path();
 
-function onMouseMove(event){
-	circle.position = new Point(event.point);
-	circle.fillColor = new Color(event.point.x/view.size.width, event.point.y/view.size.height, 1);
-	circle.bounds.height = (event.point.y+event.point.x)/4;
-	circle.bounds.width = (event.point.y+event.point.x)/4;
 
-	text.point = new Point(event.point.x-200, event.point.y-100);
-	text.content = '(x: ' + event.point.x + ', y: ' + event.point.y + ')';
+
+// stylize: strokeWidth and strokeColor
+//*** your code ***//
+path.strokeWidth = 10;
+path.strokeColor = 'blue';
+
+
+
+// add points to the path, use for-loop
+//*** your code ***//
+for(var i=0; i<x.length; i++){
+	path.add(new Point(x[i], y[i]/2));
+}
+
+
+
+// make it smooth and selected
+path.smooth({ type: 'continuous' });
+path.selected = true;
+
+var segment = null; // selected segment
+var hitOptions = {
+	segments: true, // will select segments
+	stroke: false,  // will not select stroke
+	fill: false,	// will not select shape/raster
+	tolerance: 10
+};
+
+// when we click, we select an item
+function onMouseDown(event) {
+	segment = null;
+	var hitResult = project.hitTest(event.point, hitOptions);
+	// if clicked on empty space
+	if (!hitResult) return;
+	// if clicked on an item
+	else segment = hitResult.segment;
+}
+
+function onMouseDrag(event) {
+    // if a segment is cicked and dragged
+	if (segment) {
+
+	    // change the y position of the segment
+	    // use segment.point.y
+		//*** your code ***//
+		segment.point.y = event.point.y;
+
+
+
+
+		path.smooth({ type: 'continuous' });
+	} 
 }
